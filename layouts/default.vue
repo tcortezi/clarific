0da8fec8d15621a5668275809927ebc6731a2f4c
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2020-02-16 10:03:30 -0300

    Insere elemento nuxt-link no logo posicionado no menu.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,9 +3,9 @@
     <canvas ref="canvas"></canvas>
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="/">
+        <nuxt-link class="navbar-item" to="/">
           <img src="~/assets/logo.svg">
-        </a>
+        </nuxt-link>
 
         <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
